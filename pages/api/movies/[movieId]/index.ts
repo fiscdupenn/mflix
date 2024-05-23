9a6583818fdb92136f54c13c759bfee30b791aba
Ashra <arash.ashrafnejad@gmail.com>
2024-05-23 22:21:10 +0600

    Dynamic route added

--- pages/api/movies/[movieId]/index.ts
+++ pages/api/movies/[movieId]/index.ts
@@ -1,16 +1,17 @@
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "../../../../lib/mongodb";
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import { ObjectId } from 'mongodb';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+
+    const { movieId } = req.query;
+    console.log("id get from movieId ",movieId);
     try {
         const client = await clientPromise;
         const db = client.db("sample_mflix");
         const movies = await db
             .collection("movies")
-            .find({})
-            .sort({ metacritic: -1 })
-            .limit(10)
-            .toArray();
+            .find({ _id: new ObjectId(movieId as string) }).toArray();
         res.json(movies);
     } catch (e) {
         console.error(e);
